4f40c254b71d14a95d5d144c83cc79bafcc324cf
Long Do <longdt12@onemount.com>
2022-05-11 16:18:14 +0700

    NB-5 Add Item Survey Slider

---
+++ app/src/main/java/co/nimble/lee/assignment/ui/screens/home/SurveyViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package co.nimble.lee.assignment.ui.screens.home
+
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+class SurveyViewPagerAdapter: PagerAdapter() {
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        TODO("Not yet implemented")
+    }
+}
